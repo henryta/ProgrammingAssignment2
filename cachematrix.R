3f91e7b3ff777f9029d40aa88b83a322a44325e1
henryta <henry.trevor.a@gmail.com>
2015-06-21 02:47:16 -0400

    R Programming Assignment #2
    Demonstration of the use of Lexical Scoping to maintain value of a
    variable between function calls

--- cachematrix.R
+++ cachematrix.R
@@ -8,15 +8,48 @@
 ##  set_matrix_inv - assigns the matrix inverse value to the 'cache'
 ##  get_matrix_inv - retreives the matrix inverse value which was 'cached'
 
+## Example of use of functions:
+## Create a matrix eg: 
+##    mymatrixB <- matrix(c(4,3,2,1),nrow=2,ncol=2)
+## Create Vector of functions by passing matrix to makeCacheMatrix eg:
+##    matrix_functions <- makeCacheMatrix(mymatrixB)
+## Pass the list of functions to cacheSolve to calculate inverse matrix eg:
+##    cacheSolve(matrix_functions)
+## Other matrices can now be calculated by calling set_matrix() function eg:
+##    matrix_functions$set_matrix(mymatrixA) 
+
+#############Sample Output########################
+##  > mymatrixB = matrix(c(4,3,2,1),nrow=2,ncol=2)
+##  > matrix_functions <- makeCacheMatrix(mymatrixB)
+##  > cacheSolve(matrix_functions)
+##        [,1] [,2]
+##  [1,] -0.5    1
+##  [2,]  1.5   -2
+##  > cacheSolve(matrix_functions)
+##  retrieving inverse matrix from cached data..
+##        [,1] [,2]
+##  [1,] -0.5    1
+##  [2,]  1.5   -2
+##  > matrix_functions$set_matrix(mymatrixB)
+##  matrix has not changed..
+##  > cacheSolve(matrix_functions)
+##  retrieving inverse matrix from cached data..
+##        [,1] [,2]
+##  [1,] -0.5    1
+##  [2,]  1.5   -2
+
+
+
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
   
         set_matrix <- function(y) {
           ## check to verify if the matrix being passed is the same as that in cache.
+          ## This ensures that even if executed several times with the same matrix
           if (!(is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y))) {
-                x <<- y
-                inv <<- NULL
+                x <<- y        ## this makes input matrix available to all functions
+                inv <<- NULL   ## reset inverse matrix vaiable when matrix changes
                 m <- message("new matrix..")
       
           } else {
@@ -40,14 +73,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## to cache only if input matrix has changed.Otherwise inverse matrix in cache is returned.
 
 cacheSolve <- function(list_of_funcs, ...) {
+        ## retrieve current value of inverse matrix from cache
         inv <- list_of_funcs$get_matrix_inv()
         
-        ## check to see if the inverse matrix variable is NULL.
+        ## check if the inverse matrix variable is NULL.
         if(!is.null(inv)) {
             message("retrieving inverse matrix from cached data..")
             return(inv)
         }
         
+        ##
         data <- list_of_funcs$get_matrix()
         inv <- solve(data,...)
         list_of_funcs$set_matrix_inv(inv)
